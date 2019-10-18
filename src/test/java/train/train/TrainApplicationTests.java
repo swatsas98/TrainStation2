4a6299c57c39f9a695c94ed55ec77a0fddeee7e8
swatsas98 <swat.sas.98@gmail.com>
2019-10-18 06:28:27 +0600

    Hello world

---
+++ src/test/java/train/train/TrainApplicationTests.java
@@ -0,0 +1,26 @@
+package train.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class TrainApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
